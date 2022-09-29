0bc00620b9a4e81d7831c9b5bb938c1b45976042
(Jared) llamaman248 <79767738+llamaman248@users.noreply.github.com>
2022-09-29 16:22:39 -0600

    Finished translating from my C++ sound thing for use in this java thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedCode/curveInterpolator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedCode/curveInterpolator.java
@@ -30,13 +30,6 @@ public class curveInterpolator {
 
 
 
-
-
-    Consumer<Integer> test = p -> {
-        p += 2;
-        return;
-    };
-
     interface NextPointFunction
     {
         weightedPoint run();
@@ -47,12 +40,14 @@ public class curveInterpolator {
         double run(weightedPoint prevPoint, weightedPoint nextPoint, double xPosBetween);
     }
 
-    public double linearInterpolator(dPoint p1, dPoint p2, double xPosBetween)
+    public static double linearInterpolator(dPoint p1, dPoint p2, double xPosBetween)
     {
         double percentP2 = (xPosBetween - p1.time) / (p2.time - p1.time);
         return (percentP2 * p2.value + (1.0 - percentP2) * p1.value);
     }
-
+    public static final InterpolationFunction LlinearInterpolator = (prevPoint, nextPoint, xPosBetween) -> {
+        return linearInterpolator(prevPoint, nextPoint, xPosBetween);
+    };
 
     public double curveStretchFactor = 1.0;
     boolean firstTime = true;
@@ -60,7 +55,9 @@ public class curveInterpolator {
     weightedPoint nextPoint;
 
     public double waveOffset = 0.0;
-    public NextPointFunction fToGiveNextPoint;
+    public NextPointFunction fToGiveNextPoint = () -> {
+        return new weightedPoint(0.0, 0.0, 1);
+    };
     public InterpolationFunction interpolationFunction;
 
     curveInterpolator(NextPointFunction initFuncToGiveNextPoint, InterpolationFunction initInterpolationFunc)
@@ -68,6 +65,33 @@ public class curveInterpolator {
         fToGiveNextPoint = initFuncToGiveNextPoint;
         interpolationFunction = initInterpolationFunc;
 
+    }
+
+    double nextFrame(double nextFrameDistance) {
+        if (firstTime) {
+            nextPoint = fToGiveNextPoint.run();
+            if (nextPoint.time == 0.0) {
+                prevPoint.value = nextPoint.value;
+                //double temp = nextPoint.time; (these two commented lines would be useless because they are for when time is not 0.0 on nextPoint)
+                nextPoint = fToGiveNextPoint.run();
+                //nextPoint.time += temp; // To make the Time in the function a releative offset
+            }
+
+            firstTime = false; // Setting firstTime back to true manualy may not result in expected results because the fToGiveNextPoint is not reset
+            return prevPoint.value;
+        }
+        waveOffset += curveStretchFactor * nextFrameDistance;
+
+
+        while(waveOffset > nextPoint.time)
+        {
+            // If waveOffset is not in current domain, get the next one and check again
+            prevPoint = nextPoint;
+            double temp = nextPoint.time;
+            nextPoint = fToGiveNextPoint.run();
+            nextPoint.time += temp; // To make the function return a relative time offset
+        }
+        return interpolationFunction.run(prevPoint, nextPoint, waveOffset);
     }
 
 }
