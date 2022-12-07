e93e95ec653140b7e8321bc8314e368cfc506e4f
BlackOps10373 <98848243+BlackOps10373@users.noreply.github.com>
2022-12-06 23:43:48 -0700

    Broken Zip Chain Code (Jacob)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZipChainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZipChainTest.java
@@ -3,11 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.SharedCode.Button;
 
-    @TeleOp(name = "ZipChainTest", group = "TeleOp")
+import java.util.Arrays;
+
+@TeleOp(name = "ZipChainTest", group = "TeleOp")
     public class ZipChainTest extends LinearOpMode {
 
         Button btnX = new Button();
@@ -20,6 +23,7 @@ import org.firstinspires.ftc.teamcode.SharedCode.Button;
 
             DcMotor zipChainRight = hardwareMap.get(DcMotor.class, "zipChainRight");
             zipChainRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
             zipChainRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
@@ -27,8 +31,40 @@ import org.firstinspires.ftc.teamcode.SharedCode.Button;
             waitForStart();
             while (opModeIsActive())
             {
+                if(gamepad1.a) {
+                    int[] ticks = new int[2];
+                    zipChainLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    zipChainRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-                //zipChain.setPower(gamepad1.right_stick_y);
+                    zipChainLeft.setTargetPosition(-5000);
+                    zipChainRight.setTargetPosition(-5000);
+                    zipChainLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    zipChainRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                    while (zipChainLeft.isBusy() || zipChainRight.isBusy()) {
+
+                        ticks[0] = Math.abs(zipChainLeft.getTargetPosition() - zipChainLeft.getCurrentPosition());
+                        ticks[1] = Math.abs(zipChainRight.getTargetPosition() - zipChainRight.getCurrentPosition());
+                        Arrays.sort(ticks);
+
+                        zipChainLeft.setPower(Math.abs(zipChainLeft.getTargetPosition() - zipChainLeft.getCurrentPosition()) / ticks[1]);
+                        zipChainRight.setPower(Math.abs(zipChainRight.getTargetPosition() - zipChainRight.getCurrentPosition()) / ticks[1]);
+
+                        zipChainLeft.setPower(0);
+                        zipChainRight.setPower(0);
+                    }
+
+                }
+
+
+
+                zipChainLeft.setPower(gamepad1.right_stick_y);
+                zipChainRight.setPower(gamepad1.right_stick_y);
+                telemetry.addData("Top Tick:", zipChainLeft.getCurrentPosition());
+                telemetry.addData("Bottom Tick:", zipChainRight.getCurrentPosition());
+                telemetry.update();
+
+               /* //zipChain.setPower(gamepad1.right_stick_y);
 
                 if(btnX.OnButtonDown(gamepad1.x))
                 {
@@ -59,6 +95,8 @@ import org.firstinspires.ftc.teamcode.SharedCode.Button;
                 telemetry.addData("TicksRight", zipChainRight.getCurrentPosition());
                 telemetry.update();
                 //driveTrain.move(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+                */
             }
         }
     }
